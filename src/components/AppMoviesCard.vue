8a4792acaa822d5ede4f36e8be49c0c30957d23f
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-04-17 18:45:07 +0200

    feat: add images for the cover movies

--- src/components/AppMoviesCard.vue
+++ src/components/AppMoviesCard.vue
@@ -19,7 +19,9 @@
                 } else if (this.moviesList.original_language === 'es') {
                     return new URL(`../assets/flag/spain-flag.png`, import.meta.url).href
                 }
-            }
+            },
+
+           
         }
     }
 
@@ -27,7 +29,11 @@
 
 <template>
     <div class="container-card">
-        <div class="image-placeholder"></div>
+        <img 
+        class="cover-movie" 
+        :src="'https://image.tmdb.org/t/p/w342' + moviesList.poster_path" 
+        v-if="moviesList.poster_path" 
+        :alt="moviesList.title">
         <div class="info-container">
             <h3>{{ moviesList.title }}</h3>
             <h3>{{ moviesList.original_title }}</h3>
@@ -49,16 +55,17 @@
         justify-content: center;
         align-items: center;
         text-align: center;
-        padding: 20px;
         width: calc((100% / 5) - 10px);
 
-        .image-placeholder {
-        width: 100%;
-        height: 120px;
-        border: 2px solid red;
+        .cover-movie {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            object-position: top;
         }
 
         .info-container {
+            padding: 10px;
             img {
                 width: 20px;
             }
